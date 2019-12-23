2999e43dce9110e783806cffed936895bf2095aa
to101380 <to101380@gmail.com>
2019-12-23 16:53:39 +0800

    201912231653

--- static/JS/main.js
+++ static/JS/main.js
@@ -49,7 +49,8 @@ if (typeof web3 !== 'undefined') {
       web3 = new Web3(web3.currentProvider);
       console.log("Web3連接成功"); 
       $(".connect").css("background-color","#6ab76a");
-      $("#connect").text("已連結");       
+      $("#connect").text("已連結");  
+      $("#link_sucess").text("已連結");    
     } else {
       // Set the provider you want from Web3.providers
       web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545")); 
@@ -71,12 +72,13 @@ if (typeof web3 !== 'undefined') {
       var balance = await web3.eth.getBalance(coinbase);
 
       $("#my_address").text(coinbase);
+      $("#redundant_user").text(coinbase);
       $("#my_balance").text(toPercent_A(web3.utils.fromWei(balance)));
       $("#balance_info").text(toPercent_A(web3.utils.fromWei(balance)));  //wei 轉換成 ether web3.utils.fromWei() 
 
 
-      contract_address = "0x7b9491DB88872423533823112F847c84C324bF0E";
-      var contract_abi = [{"constant":true,"inputs":[],"name":"count","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"son","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"Safe_trans","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_son","type":"uint256"},{"name":"_mon","type":"uint256"}],"name":"Set_quota","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"total_count","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"distribute","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"usage_count","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"userinfo","outputs":[{"name":"amount","type":"uint256"},{"name":"user_profit","type":"uint256"},{"name":"block_number","type":"uint256"},{"name":"timestamp","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"Dividing_times","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"querybalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"key","type":"uint256"}],"name":"Set_Interest","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getInterest","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"mon","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"invest","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"Amount_invested","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"quit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"interest","type":"uint256"}],"name":"invest_act","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"user","type":"address"}],"name":"end","type":"event"}];
+      contract_address = "0x1234c5B2898CCc07b8aB5AC411225f071cA9d345";
+      var contract_abi = [{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"propose","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"count","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"son","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_son","type":"uint256"},{"name":"_mon","type":"uint256"}],"name":"Set_quota","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"quite_user","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"total_count","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"volume","type":"uint256"}],"name":"Safe_trans_B","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"Safe_trans_A","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"distribute","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"usage_count","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"verification","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"userinfo","outputs":[{"name":"amount","type":"uint256"},{"name":"user_profit","type":"uint256"},{"name":"block_number","type":"uint256"},{"name":"timestamp","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"Dividing_times","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"querybalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"key","type":"uint256"}],"name":"Set_Interest","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"Recommend","type":"address"}],"name":"recommend","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getInterest","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"mon","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"invest","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"Amount_invested","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"quit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"interest","type":"uint256"}],"name":"invest_act","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"recommend","type":"address"},{"indexed":false,"name":"recommended","type":"address"}],"name":"Recommended","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"user","type":"address"}],"name":"end","type":"event"}];
       contract_ad();
       myContract = new web3.eth.Contract(contract_abi, contract_address);
 
@@ -87,11 +89,13 @@ if (typeof web3 !== 'undefined') {
       var Interest = await myContract.methods.getInterest().call({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'});
       $("#Interest_number").text(toPercent_01(1/Interest));
       $("#profit_year").text(toPercent_01((1/Interest)*365));
+      $("#ready_interest").text(toPercent_01(1/Interest));
+      $("#ready_year").text(toPercent_01((1/Interest)*365));
 
       var count = await myContract.methods.count().call({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'});
       $("#count").text(count);   
 
-      var user_info = await myContract.methods.userinfo(coinbase).call({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'});
+      var user_info = await myContract.methods.userinfo(coinbase).call({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'});      
       $("#capital").text(web3.utils.fromWei(user_info[0]));
       $("#user_interest").text(toPercent_01(1/user_info[1]));     
       block_height = user_info[2];     
@@ -101,7 +105,27 @@ if (typeof web3 !== 'undefined') {
 
       if(user_info[0] != 0){
         $(".sible").css("visibility","visible");
+        $(".investor_info").css("visibility","visible");
+        $(".none_user").css("display","none");  
       }
+
+      var propose = await myContract.methods.propose(coinbase).call({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'});
+      $("#my_propose").text(propose);
+
+      if (propose == "0x0000000000000000000000000000000000000000") {
+        $("#my_propose").text("沒有推薦人");
+      }
+
+      var repeat = await myContract.methods.verification(coinbase).call({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'});
+      if( repeat != "0x0000000000000000000000000000000000000000" ){
+        $(".redundant").css("visibility ","visible");
+        $(".no_duplication").css("display","none");
+      }else{
+        $(".redundant").css("display","none");
+        $(".no_duplication").css("visibility ","visible");
+      }
+
+
 
       var son = await myContract.methods.son().call({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'});
       var mon = await myContract.methods.mon().call({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'});
@@ -115,27 +139,42 @@ if (typeof web3 !== 'undefined') {
 
     printPostsToConsole();
 
-
     //退出
     function quite(){
       myContract.methods.quit().send({from: coinbase}).then(function(receipt){          
         location.reload();
       });
     } 
+
+     var comfirm =  document.querySelector('#comfirm');
+      comfirm.addEventListener("click",function(e){
+        e.preventDefault();
+        var count = document.querySelector('.quantity').value;        
+        $("#ready_volume").text(count);
+
+      })
  
     //input接收輸入的值，並傳到錢包
      var pay = document.querySelector('#invest');
       pay.addEventListener("click", function(e) {
         e.preventDefault();
-        var volume = document.querySelector('.quantity').value;
-        var volume = (Number(volume) * 1000000000000000000).toString();      
+        var count = document.querySelector('.quantity').value;
+        var volume = (Number(count) * 1000000000000000000).toString();
+        var recommended = document.querySelector('.propose').value;        
         invest(volume);
+        recommend(recommended);
       });
 
     
 
     function invest(volume){
       myContract.methods.invest().send({from: coinbase , value: volume}).then(function(receipt){          
+        location.reload();
+      });
+    }
+
+    function recommend(recommended){
+      myContract.methods.recommend(recommended).send({from: coinbase}).then(function(receipt){          
         location.reload();
       });
     }
@@ -164,11 +203,20 @@ if (typeof web3 !== 'undefined') {
 
 
     function myload() {
-      $(".loader").fadeOut(3000);
-      $(".load").text("正在載入中");
+      $(".loader").fadeOut(3000);      
     }
-
     window.onload = myload;   
- 
+
+
+ // Global site tag (gtag.js) - Google Analytics 
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-154888716-1');
+
+
+  
+
           
 
